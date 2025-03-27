443788669aafa162afc3998fc0bcb72f574d213b
bancell04 <118390054+bancell04@users.noreply.github.com>
2025-03-27 16:36:17 -0600

    added movement animations

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -8,6 +8,30 @@ func _physics_process(delta):
 	motion.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	motion.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	motion.y /= 2
+	
+	if (motion.x > 0):
+		if (motion.y > 0):
+			$AnimatedSprite2D.play("walking_down_right")
+		elif (motion.y < 0):
+			$AnimatedSprite2D.play("walking_up_right")
+		elif (motion.y == 0):
+			$AnimatedSprite2D.play("walking_right")
+	elif (motion.x < 0):
+		if (motion.y > 0):
+			$AnimatedSprite2D.play("walking_down_left")
+		elif (motion.y < 0):
+			$AnimatedSprite2D.play("walking_up_left")
+		elif (motion.y == 0):
+			$AnimatedSprite2D.play("walking_left")
+	elif (motion.x == 0):
+		if (motion.y > 0):
+			$AnimatedSprite2D.play("walking_down")
+		elif (motion.y < 0):
+			$AnimatedSprite2D.play("walking_up")
+		elif (motion.y == 0):
+			$AnimatedSprite2D.play("idle")
+			 
+			
 	velocity = motion.normalized() * MOTION_SPEED
 	
 	print(velocity.x)
