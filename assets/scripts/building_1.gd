d63dbaa32c2085cf785a835ab5e6022f345c7a2e
bancell04 <bancell@mines.edu>
2025-04-03 09:48:20 -0600

    add files

---
+++ assets/scripts/building_1.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+@onready var roof = $layer_5
+@onready var walls = [$layer_1, $layer_2, $layer_3, $layer_4]
+#@onready var overlay = get_tree().get_root().get_child(0).find_node("Overlay", true, false)
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		roof.visible = false
+		#overlay.visible = true;
+		for i in range(len(walls)):
+			if i == 0:
+				walls[i].modulate.a = 0.4
+			else:
+				walls[i].visible = false
+				walls[i].collision_enabled = false
+
+			
+
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		#overlay.visible = false;
+		roof.visible = true
+		for layer in walls:
+			layer.modulate.a = 1
+			layer.visible = true 
+			layer.collision_enabled = true 
+		
