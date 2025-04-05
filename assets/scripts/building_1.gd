214d95a8bcfabb90e3617e6f541d68cf24c8d38f
bancell04 <bancell@mines.edu>
2025-04-04 19:36:52 -0600

    working overlay

--- assets/scripts/building_1.gd
+++ assets/scripts/building_1.gd
@@ -2,12 +2,12 @@ extends Node2D
 
 @onready var roof = $layer_5
 @onready var walls = [$layer_1, $layer_2, $layer_3, $layer_4]
-#@onready var overlay = get_tree().get_root().get_child(0).find_node("Overlay", true, false)
+@onready var overlay = get_tree().get_root().get_node("Game/Player/Camera2D/Overlay")
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		roof.visible = false
-		#overlay.visible = true;
+		overlay.visible = true;
 		for i in range(len(walls)):
 			if i == 0:
 				walls[i].modulate.a = 0.4
@@ -21,7 +21,7 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.name == "Player":
-		#overlay.visible = false;
+		overlay.visible = false;
 		roof.visible = true
 		for layer in walls:
 			layer.modulate.a = 1
